bc032472df90d46268a204e5d8342988b4841bc8
Ikenna Nwaiwu <ikenna4u@gmail.com>
2023-11-18 13:45:50 +0000

    Chapter9

--- chapter9/check_cluster_status.sh
+++ chapter9/check_cluster_status.sh
@@ -2,6 +2,6 @@
 kubectl get pods
 kubectl get pods -o=jsonpath='{.items[0].metadata.name}'
 POD_NAME=$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}')
-echo "POD_NAME=$POD_NAME"
+echo "\nPOD_NAME=$POD_NAME"
 kubectl wait --for=condition=Ready pod/$POD_NAME --timeout=60s
 kubectl logs $POD_NAME --tail=20
