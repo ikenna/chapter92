8ff8eaeba92ed81dbcab34a6b46eb357944bd404
Ikenna Nwaiwu <ikenna4u@gmail.com>
2023-11-19 03:00:55 +0000

    Chapter9

--- chapter9/check_cluster_status.sh
+++ chapter9/check_cluster_status.sh
@@ -5,6 +5,5 @@ kubectl get pods -o=jsonpath='{.items[0].metadata.name}'
 POD_NAME=$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}')
 kubectl wait --for=condition=Ready pod/$POD_NAME --timeout=60s
 kubectl logs $POD_NAME --tail=20
-kubectl wait --for=condition=Ready deployment/product-api
 curl --location 'http://localhost:8000/v1/catalog/products/1a7322e3-df81-8362-998c-50c67c6f8a15' \
 --header 'Accept: application/vnd.api+json'
